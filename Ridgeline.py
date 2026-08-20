451977d63581e2ab52cc51b0f4c1e16a3cad9d41
FlynnM1311 <flynn.munro@sbhsstudent.school.nz>
2026-08-21 11:07:12 +1200

    Created Tkinter window.
    Created Tkinter window.

--- Ridgeline.py
+++ Ridgeline.py
@@ -1,2 +1,15 @@
-import tkinter
+#
+# imports
+import tkinter as tk
+from tkinterweb import HtmlFrame
 import folium
+import os
+import tempfile
+
+root = tk.Tk()
+
+root.title("Ridgeline app")
+
+root.geometry("800x600")
+
+root.mainloop()
