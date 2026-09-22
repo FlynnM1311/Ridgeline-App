a7dffdb4ec3e05ad780c5cab36a2a5ed51111fe4
FlynnM1311 <flynn.munro@sbhsstudent.school.nz>
2026-09-22 12:03:46 +1200

    Started the vars for the storage section

--- Main work/data/storage.py
+++ Main work/data/storage.py
@@ -2,3 +2,18 @@ import sqlite3
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Optional
+
+#Making sure data storage
+DB_PATH = Path(__file__).parent / "ridgeline.db"
+
+@dataclass
+class Listing:
+    address: str
+    email: str
+    latitude: float
+    longitude: float
+    description: str = ""
+    acreage: Optional[float] = None
+    id: Optional[int] = field(default=None)
+ 
+ 
