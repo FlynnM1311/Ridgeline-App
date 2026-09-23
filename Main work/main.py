ec05f21591b5507dff8f87e06636fd869825f5ba
FlynnM1311 <flynn.munro@sbhsstudent.school.nz>
2026-09-23 23:29:14 +1200

    Switched from tk webview

--- Main work/main.py
+++ Main work/main.py
@@ -1,24 +1,30 @@
 #M
-import clr
- 
-clr.AddReference("System.Windows.Forms")
-clr.AddReference("System.Threading")
- 
-from System.Threading import ApartmentState, Thread, ThreadStart
+import ctypes
  
 from gui.main_window import MainWindow
-def run_app() -> None:
-    from tkwebview2.tkwebview2 import have_runtime, install_runtime
  
-    if not have_runtime():
-        install_runtime()
+try:
+    ctypes.windll.shcore.SetProcessDpiAwareness(1)  # DPI_AWARENESS_SYSTEM_AWARE
+except Exception:
+    pass
 
+if __name__ == "__main__":
     app = MainWindow()
     app.mainloop()
  
+#from gui.main_window import MainWindow
+#def run_app() -> None:
+    #from tkwebview2.tkwebview2 import have_runtime, install_runtime
  
-if __name__ == "__main__":
-    thread = Thread(ThreadStart(run_app))
-    thread.ApartmentState = ApartmentState.STA
-    thread.Start()
-    thread.Join()
+    #if not have_runtime():
+        #install_runtime()
+
+    #app = MainWindow()
+    #app.mainloop()
+ 
+ 
+#if __name__ == "__main__":
+    #thread = Thread(ThreadStart(run_app))
+    #thread.ApartmentState = ApartmentState.STA
+    #thread.Start()
+    #thread.Join()
